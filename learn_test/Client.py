8f7ced1c9ca5b8d1a37671385d32b902e7be7d70
lis7fire <root@gmail.com>
2017-09-29 09:42:39 +0800

    11111

--- learn_test/Client.py
+++ learn_test/Client.py
@@ -6,11 +6,29 @@ class clients(object):
 	"""docstring for clients"""
 	def __init__(self, ip_port):
 		self.csock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		# self.csock.connect(ip_port)
+		self.csock.connect(ip_port)
+	def send_meg(self,meg):
+		return self.csock.send(meg.encode('utf-8'))
+		pass
+	def get_meg(self):
+		return self.csock.recv(1024)
+		pass
 
 
-c=clients(('127.0.0.1',8001))
-c.csock.connect(('127.0.0.1',8003))
-c.csock.send(b'Luckyv')
-print((c.csock.recv(1024)).decode('utf-8'))
-c.csock.close()
+c=clients(('127.0.0.1',8002))
+c.send_meg('奥特曼')
+print('收到的消息：',c.get_meg().decode('utf-8'))
+while True:
+	try:
+		c.send_meg(input('请输入：'))
+		info=c.get_meg().decode('utf-8')
+		print('收到的消息：',info)
+		if info=='你好，exit':
+			c.csock.close()
+			print('连接已断开！')
+			break
+			pass
+	except Exception as e:
+		c.csock.close()
+		raise e
+print('远程服务器关闭了！')
--- learn_test/Server.py
+++ learn_test/Server.py
@@ -9,19 +9,47 @@ class server(object):
 		self.ser.bind(('0.0.0.0',8000))
 		self.ser.listen(9)
 		print('Waiting for connecting.........')
-
-# s=server('服务器')
-ser=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ser.bind(('0.0.0.0',8003))
-ser.listen(9)
-while True:
-	print('开始监听链接。。。')
-	sock,addr=ser.accept()
-	data=sock.recv(1024)
-	print('客户端ip：',addr,' 你好，',data)
-	send_meg='我是服务器127'+data.decode('utf-8')
-	sock.send(send_meg.encode('utf-8'))
-	sock.send('hhhhhhh'.encode('utf-8'))
-	sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
-	sock.close()
+	def send_meg(self,mesg):
+		self.ser.send(mesg.encode('utf-8'))
+		pass
+	def get_data(self):
+		pass
+	def get_ser(self):
+		return self.ser
+		pass
+def send_meg(mesg,sock):
+	sock.send(mesg.encode('utf-8'))
 	pass
+def get_meg(length,sock):
+	return sock.recv(length).decode('utf-8')
+	pass
+def exitus(data,sock):
+	send_meg('你关闭连接了！',sock)
+	send_meg('exit',sock)	
+	sock.close()
+	if data=='exitall':
+		ser.close()
+		return True
+	return False
+
+ser=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ser.bind(('0.0.0.0',8002))
+ser.listen(9)
+print('服务器正在监听.....')
+print('开始监听连接...')
+sock,addr=ser.accept()
+while True:
+	try:
+		data=get_meg(1024,sock)
+		print('客户端ip：',addr,'收到的消息：',data)
+		strs='你好，'+data
+		send_meg(strs,sock)
+		if data=='exit' or data=='exitall':
+			# if exitus(data,sock):break
+			break
+	except Exception as e:
+		sock.close()
+		ser.close()	
+		raise e
+print('服务器已关闭！！！')
+
